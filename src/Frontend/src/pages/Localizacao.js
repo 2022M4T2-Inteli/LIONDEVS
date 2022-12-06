9b22ea5928d797f4ebd7b7e7ee2dcc4341114eb3
Lucas Conti <lucas.pereira@sou.inteli.edu.br>
2022-12-06 15:40:53 -0300

    Estilo

--- src/Frontend/src/pages/Localizacao.js
+++ src/Frontend/src/pages/Localizacao.js
@@ -9,6 +9,7 @@ import Select, { SelectChangeEvent } from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 // console.log(logo);
 
@@ -54,14 +55,16 @@ function Localizacao() {
             </Select>
             </FormControl>
       </div>
+      <Box sx={{ width:'430px', height: '180px', mt:'3rem', ml:'25rem', backgroundColor: '#DCDCDC', borderRadius:'25px'}}>
       <div>
-      <Button sx={{width:'188px', height:'56.93px', mt:'1rem', ml:'18rem', backgroundColor:'#34C471', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Adicionar</Button>
-      <Button sx={{width:'188px', height:'56.93px', mt:'1rem', ml:'16rem', backgroundColor:'#34C471', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Revelar tudo</Button>
+      <Button sx={{width:'188px', height:'56.93px', mt:'1rem', ml:'1rem', backgroundColor:'#34C471', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Adicionar</Button>
+      <Button sx={{width:'188px', height:'56.93px', mt:'1rem', ml:'1rem', backgroundColor:'#34C471', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Revelar tudo</Button>
       </div>
       <div>
-      <Button sx={{width:'188px', height:'56.93px', mt:'2rem', ml:'18rem', backgroundColor:'#E64747', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Remover</Button>
-      <Button sx={{width:'188px', height:'56.93px', mt:'2rem', ml:'16rem', backgroundColor:'#E64747', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Remover tudo</Button>
+      <Button sx={{width:'188px', height:'56.93px', mt:'2rem', ml:'1rem', backgroundColor:'#E64747', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Remover</Button>
+      <Button sx={{width:'188px', height:'56.93px', mt:'2rem', ml:'1rem', backgroundColor:'#E64747', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Remover tudo</Button>
       </div>
+      </Box>
       <img src={logo} alt="Logo" width="1200" height="700"/>
     </>
   );
