f85dcd5a777224541b59fd5d4f1106ebbd23d9ed
Alexandre Fonseca <80794067+Xandebrabe@users.noreply.github.com>
2022-12-12 09:12:29 -0300

    Pequena refatoração

--- src/Frontend/src/pages/Home.js
+++ src/Frontend/src/pages/Home.js
@@ -1,34 +1,12 @@
 import NavigationBar from "../components/navbar";
 import Button from '@mui/material/Button';
 import { Typography } from "@mui/material";
-import { fetchData } from '../services/AwsFunction';
 import Graphicss from "./Graphics";
 
 function Home() {
-        // constructor(props) {
-        //     super(props);
-        //     this.state = { apiResponse: "" };
-        // }
-
-        // callAPI() {
-        //     fetch("http://localhost:9000/testAPI")
-        //     .then(res => res.text())
-        //     .then(res => this.setState({ apiResponse: res }));
-        // }
-
-        // componentWillMount() {
-        //     this.callAPI();
-        // }
-    // render(){
-        const fetchDataFormDynamoDb = () => {
-            fetchData('rfidTable');
-          }
-        // const information = fetchData('rfiTable');
         return (
             <>
             <NavigationBar></NavigationBar>
-            {/* <p>{fetchData.luminosidade}</p> */}
-            {/* <p>{fetchData.items[1].localizacao}</p> */}
             <Graphicss></Graphicss>
             <div>
                     <Button sx={{bgcolor:"#0082AF", borderRadius:'90px', width:'337px', height:'69px',display:'inline-block', mt:'20px', ml:'95px' }}>
@@ -41,7 +19,7 @@ function Home() {
                             Rastrear Dispositivos
                         </Typography>
                     </Button>
-                    <Button sx={{bgcolor:"#0082AF", borderRadius:'90px', width:'337px', height:'69px', display:'inline-block', mt:'20px', ml:'95px'}} onClick={() => fetchDataFormDynamoDb()}>
+                    <Button sx={{bgcolor:"#0082AF", borderRadius:'90px', width:'337px', height:'69px', display:'inline-block', mt:'20px', ml:'95px'}}>
                         <Typography sx={{color:'#FFFFFF'}}>
                             Gerar Relatórios
                         </Typography>
