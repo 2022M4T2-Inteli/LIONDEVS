7e3269c8fe87a57fe5bdf1ac90d597c66d862a91
Alexandre Fonseca <80794067+Xandebrabe@users.noreply.github.com>
2022-12-14 15:00:24 -0300

    Adding Table to the location page

--- src/Frontend/src/pages/Localizacao.js
+++ src/Frontend/src/pages/Localizacao.js
@@ -14,9 +14,10 @@ import DataTable from "../components/Table_Home";
 
 function Localizacao() {
 
+const [id, createId] = useState('')
+
 async function getDevices() {
     await axios.get('http://Testeapi-env.eba-x4bgfctn.us-east-1.elasticbeanstalk.com/product/all').then( async res => { 
-      console.log(res.data.products)
       return setData(res.data.products) 
     } );
   }
@@ -30,30 +31,25 @@ async function getDevices() {
 
   return (
     <>
-    
-   
       <NavigationBar></NavigationBar>
       <br></br>
       <br></br>
 
       <div sx={{ display: 'inline' }}>
         <Typography sx={{ ml: '15rem', fontSize: '24px', height: '10px', display: 'inline' }}>Pesquisa por ID</Typography>
-        {/* <TextField label="ID do Dispositivo"
-          id="outlined-size-small"
-          defaultValue="ID"
-          size="small" style={{ padding: 10 }} sx={{ ml: '300px', height: '50px', borderRadius: '36px', display: 'inline' }} /> */}
       </div>
 
       <div sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-      
+
         <div sx={{ display: 'inline', height: '2px'}}>
           <Typography sx={{ ml: '15rem', mt:'2rem', fontSize: '24px', height: '10px', width: 'fit-content' }}>Pesquisa por tipo</Typography>
         </div>
 
         <TextField size="small" label="ID" variant="outlined" sx={{ width:'514px', height:'55px', ml:'450px', mt: '-80px'}} />
 
-        <FormControl sx={{ minWidth: 515 , ml: '450px', mt: '-30px'}} size="small">
+          <FormControl sx={{ minWidth: 515 , ml: '450px', mt: '-30px'}} size="small">
             <InputLabel id="demo-select-small">Tipo</InputLabel>
+
             <Select 
             labelId="demo-select-small"
             id="demo-select-small"
@@ -65,11 +61,14 @@ async function getDevices() {
             <MenuItem value={20}>Tablet</MenuItem>
             <MenuItem value={30}>Cadeiras</MenuItem>
             <MenuItem value={40}>Mesas</MenuItem>
-
             </Select>
-            </FormControl>
+
+          </FormControl>
       </div>
+        <br />
+        <br />
       <div> 
+        <DataTable/>
         {data.map((device, index) => (
           <p key={index}>{device.tipo}</p>
         ))}
@@ -82,18 +81,20 @@ async function getDevices() {
         {data.map((device, index) => (
           <p key={index}>{device.rfid}</p>
         ))}
-        
       </div>
+
       <Box sx={{ width:'430px', height: '180px', mt:'3rem', ml:'25rem', backgroundColor: '#DCDCDC', borderRadius:'25px'}}>
-      <div>
-      <Button sx={{width:'188px', height:'56.93px', mt:'1rem', ml:'1rem', backgroundColor:'#34C471', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Adicionar</Button>
-      <Button sx={{width:'188px', height:'56.93px', mt:'1rem', ml:'1rem', backgroundColor:'#34C471', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Revelar tudo</Button>
-      </div>
-      <div>
-      <Button sx={{width:'188px', height:'56.93px', mt:'2rem', ml:'1rem', backgroundColor:'#E64747', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Remover</Button>
-      <Button sx={{width:'188px', height:'56.93px', mt:'2rem', ml:'1rem', backgroundColor:'#E64747', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Remover tudo</Button>
-      </div>
+        <div>
+        <Button sx={{width:'188px', height:'56.93px', mt:'1rem', ml:'1rem', backgroundColor:'#34C471', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Adicionar</Button>
+        <Button sx={{width:'188px', height:'56.93px', mt:'1rem', ml:'1rem', backgroundColor:'#34C471', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Revelar tudo</Button>
+        </div>
+
+        <div>
+        <Button sx={{width:'188px', height:'56.93px', mt:'2rem', ml:'1rem', backgroundColor:'#E64747', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Remover</Button>
+        <Button sx={{width:'188px', height:'56.93px', mt:'2rem', ml:'1rem', backgroundColor:'#E64747', color:'#FFFFFF', borderRadius:'36px', display:'inline'}}>Remover tudo</Button>
+        </div>
       </Box>
+
       <img src={logo} alt="Logo" width="1200" height="700"/>
     </>
   );
